c2e467caace7800766637e9eb6fb71f21502d296
Karine Gasesyan <karinegasesyan@gmail.com>
2025-02-21 13:51:45 +0400

    Fix send us message alignment

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -2,7 +2,7 @@
     <button 
         type="submit" 
         aria-label="Send us a message" 
-        class="relative flex flex-col w-full px-4 md:px-5 py-3 md:py-4 rounded-[10px] gap-2 bg-transparent border border-[var(--color-primary)] transition-all duration-300 group cursor-pointer focus:outline-none focus:ring-2 focus:ring-[var(--color-primary)]"
+        class="relative flex flex-col w-full px-4 md:px-5 py-3 md:py-4 rounded-[10px] gap-2 bg-transparent border border-[var(--color-primary)] transition-all duration-300 group cursor-pointer focus:outline-none focus:ring-2 focus:ring-[var(--color-primary)] text-left"
     >
         <p class="font-inter text-sm md:text-base font-semibold text-[var(--color-white)] leading-[14px] group-hover:text-[var(--color-white)]">
             Send us a message
